f0b901c907ff8a03fc513884b4478b2dd428c2a8
vikranth19 <vikranthpaluri@gmail.com>
2024-04-14 12:33:21 +0530

    JWT filter & utils implementation

--- src/main/java/com/springbootprojects/AssignmentSubmissionApp/config/SecurityConfig.java
+++ src/main/java/com/springbootprojects/AssignmentSubmissionApp/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.springbootprojects.AssignmentSubmissionApp.config;
 
+import com.springbootprojects.AssignmentSubmissionApp.filter.JwtFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -15,9 +21,34 @@ public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
 
+    private final JwtFilter jwtFilter;
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api").permitAll()
+                        .anyRequest().authenticated()
+        )
+        .sessionManagement(sessionManagement ->
+                sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .exceptionHandling(exceptionHandling ->
+                exceptionHandling
+                        .authenticationEntryPoint((request, response, ex) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+                        })
+        );
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 }
--- src/main/java/com/springbootprojects/AssignmentSubmissionApp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/springbootprojects/AssignmentSubmissionApp/service/UserDetailsServiceImpl.java
@@ -1,24 +1,27 @@
 package com.springbootprojects.AssignmentSubmissionApp.service;
 
 import com.springbootprojects.AssignmentSubmissionApp.domain.User;
+import com.springbootprojects.AssignmentSubmissionApp.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(passwordEncoder.encode("easypass"));
-        user.setId(1L);
-        return user;
+
+        Optional<User> user = userRepository.findByUsername(username);
+
+        return user.orElseThrow(() -> new UsernameNotFoundException("Invalid credentials"));
     }
 }
